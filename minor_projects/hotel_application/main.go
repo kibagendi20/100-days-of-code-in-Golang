c6121297feb2d78465f9f9c82a5b277eef4f3be6
Wesley Kibagendi <wesoon98@gmail.com>
2023-03-31 12:38:26 +0300

    Hotel Management Application

---
+++ minor_projects/hotel_application/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	const totalRooms = 134
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println()
+	fmt.Println()
+	var hotel string = "Gopher Paris Inn"
+	var roomsAvailable int
+	var roomLabeling int = 110
+	var occupancyLevel string
+	roomsAvailable = rand.Intn(134)
+	roomsOccupied := totalRooms - roomsAvailable
+	occupancyRate := roomsOccupied / totalRooms
+	if occupancyRate < 30 {
+		occupancyLevel = "Low"
+	} else if occupancyRate < 60 {
+		occupancyLevel = "Medium"
+	} else {
+		occupancyLevel = "High"
+	}
+
+	fmt.Print("Hotel: ", hotel)
+	fmt.Println("      Occupancy level: ", occupancyLevel)
+	fmt.Println("                             Occupancy rate: ", occupancyRate, "%")
+	fmt.Println("Number of rooms: ", totalRooms)
+	fmt.Println("Rooms available: ", roomsAvailable)
+
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	if roomsAvailable != 0 {
+		fmt.Println("Rooms:")
+		for roomsAvailable != 0 {
+			noPeople := rand.Intn(10) + 1
+			nightsAvailable := rand.Intn(10) + 1
+			if nightsAvailable == 1 {
+				fmt.Printf("- %d : %d people / %d night \n", roomLabeling, noPeople, nightsAvailable)
+			} else {
+				fmt.Printf("- %d : %d people / %d nights \n", roomLabeling, noPeople, nightsAvailable)
+			}
+
+			roomLabeling++
+			roomsAvailable--
+		}
+	} else {
+		fmt.Println("No rooms available for tonight")
+	}
+}
